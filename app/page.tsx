8dd768245b9d873d164f36747bae08eab76a72a4
GeluHorotan <horotangelu17@gmail.com>
2023-08-18 16:33:36 +0300

    feat: adding bg color on main html tag

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
 import Image from 'next/image';
 
 export default function Home() {
-  return <main className="">{/* <h1>Dyntell</h1> */}</main>;
+  return (
+    <main className="bg-red-400 w-full h-screen">{/* <h1>Dyntell</h1> */}</main>
+  );
 }
