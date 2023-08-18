b50b6e18c6fb6df068d6827968e7fd33c6243254
GeluHorotan <horotangelu17@gmail.com>
2023-08-18 15:45:11 +0300

    feat: implement the header locale into header component

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,19 +1,24 @@
 import Link from 'next/link';
+import HeaderLocale from '@/locales/HeaderLocale.json';
 
 const Header = () => {
+  const { textLogo, navItems } = HeaderLocale;
   return (
-    <nav className="bg-red-400 w-full px-7 py-3 flex items-center justify-between">
+    <nav className=" w-full px-7 py-3 flex items-center justify-between">
       <Link href={'/'} className="text-2xl font-bold uppercase tracking-wide">
-        Dyntell
+        {textLogo}
       </Link>
 
-      <ul>
-        <li>
-          <Link href="/about">About</Link>
-        </li>
-        <li>
-          <Link href="/about">CONTACT</Link>
-        </li>
+      <ul className="flex  gap-8">
+        {navItems.map((item, i) => {
+          return (
+            <li key={i}>
+              <Link className="nav_item" href={item.href}>
+                {item.text}
+              </Link>
+            </li>
+          );
+        })}
       </ul>
     </nav>
   );
