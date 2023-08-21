dbb6573f74a291ae1bee7017fec445692e6a09a6
GeluHorotan <horotangelu17@gmail.com>
2023-08-21 15:28:35 +0300

    fix: responsive issue on page not found and page under development

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -6,8 +6,8 @@ import React from 'react';
 const About: NextPage = () => {
   return (
     <div className="px-7 py-4 flex items-center justify-center h-[90vh] bg-slate-800">
-      <div className="bg-slate-200 rounded-3xl max-md:w-full  w-1/2 h-1/2 flex items-center flex-col justify-center gap-4  text-slate-800">
-        <h4 className="font-semibold">Page under development.</h4>
+      <div className="bg-slate-200 rounded-3xl max-md:w-full max-md:py-7 max-md:h-max  w-1/2 h-1/2 flex items-center flex-col justify-center gap-4  text-slate-800">
+        <h4 className="font-semibold text-center">Page under development.</h4>
         <CountdownRedirect />
       </div>
     </div>
--- app/not-found.tsx
+++ app/not-found.tsx
@@ -6,7 +6,7 @@ import { NextPage } from 'next';
 const NotFound: NextPage = () => {
   return (
     <div className="px-7 py-4 flex items-center justify-center h-[90vh] bg-slate-800">
-      <div className="bg-slate-200 rounded-3xl max-md:w-full  w-1/2 h-1/2 flex items-center flex-col justify-center gap-4 text-slate-800">
+      <div className="bg-slate-200 rounded-3xl max-md:w-full max-md:py-7 max-md:h-max w-1/2 h-1/2 flex items-center flex-col justify-center gap-4 text-slate-800">
         <h4 className="font-semibold">Page not found.</h4>
         <CountdownRedirect />
       </div>
